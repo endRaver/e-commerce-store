2568ea6c6c6a3ef0a9f847a1db82cbf25427e476
Saltypeesh <tungthanh254@gmail.com>
2025-03-25 22:54:18 +0700

    Implement signup, login, logout feature. - Enhance authentication flow by integrating cookie-based token management for access and refresh tokens. - Update dependencies to use 'cookie-parser' and add new environment variables for token secrets. - Refactor signup and login controllers to handle token generation and storage in cookies.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,33 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+
+  return { accessToken, refreshToken }
+}
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(`refresh_token:${userId}`, refreshToken, 'EX', 7 * 24 * 60 * 60)  // 7 days
+}
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,   // prevent XSS attacks,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",   // prevent CSRF attacks, cross-site request forgery attacks
+    maxAge: 15 * 60 * 1000,   // 15 minutes
+  })
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  })
+}
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -13,10 +42,16 @@ export const signup = async (req, res) => {
     const user = await User.create({ name, email, password });
 
     // authenticate user
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
 
     res.status(201).json({
-      user,
-      message: 'User created successfully',
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -24,9 +59,46 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send('Login route');
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateTokens(user._id)
+
+      await storeRefreshToken(user._id, refreshToken)
+      setCookies(res, accessToken, refreshToken)
+
+      res.json({
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+      })
+    } else {
+      res.status(401).json({ message: "Invalid email or password" })
+    }
+  } catch (error) {
+    console.log('Error in login controller', error.message);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send('Logout route');
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (refreshToken) {
+      const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+      await redis.del(`refresh_token:${decoded.userId}`);
+    }
+
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log('Error in logout controller', error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };

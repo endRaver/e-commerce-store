0ecad1a528ae480538395adec29136abe7252e7f
Saltypeesh <tungthanh254@gmail.com>
2025-03-30 00:37:33 +0700

    Update server.js to serve frontend index.html in production and add e-commerce-store dependency in package.json and package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,6 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
@@ -38,6 +37,7 @@ app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );

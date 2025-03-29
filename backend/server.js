9b11c3ac1f938892113894ccbc82dc71e07d4d96
Saltypeesh <tungthanh254@gmail.com>
2025-03-30 00:25:01 +0700

    Add development environment configuration and build script
    - Introduce NODE_ENV variable in .env for development environment.
    - Add build script in package.json to install dependencies and build frontend assets.
    - Update server.js to serve static files in production environment.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import setupSwagger from "./lib/swaggerConfig.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -19,6 +19,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
+
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
@@ -32,6 +35,13 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
 
 setupSwagger(app);
 
